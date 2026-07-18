34f1307bb21237075cfe1189c5cbfb9c5395e189
Sunil Singh <sunilsingh77@gmail.com>
2026-07-18 15:51:32 +0530

    testing done for existing functionality

--- src/app/core/models/api-error.model.ts
+++
@@ -1,9 +0,0 @@
-export interface ApiError {
-  success: boolean;
-  statusCode: number;
-  message: string;
-  errorCode?: string;
-  traceId?: string;
-  timestamp?: string;
-  errors?: Record<string, string[]>;
-}
